fd3d81d7a82f9164dae1c8c678135a35524a22f2
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-01-10 21:05:39 -0400

    refactor: implement all packages to use in actual project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  optimizeFonts: true,
+  experimental: {
+    adjustFontFallbacks: true,
+  },
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      namespace: "kurs",
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
